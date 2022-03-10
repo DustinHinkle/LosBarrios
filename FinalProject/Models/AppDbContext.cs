5535a8695828c18cd98b4684107686ee101f8982
Dustin Hinkle <Dustin.Hinkle.R@gmail.com>
2022-03-10 17:34:14 -0600

    Start

---
+++ FinalProject/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=database.db");
+        }
+
+        
+    }
+}
