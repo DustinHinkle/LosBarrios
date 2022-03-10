6d77e6d9d93b8526734a3249b356d115b12d00b6
Dustin Hinkle <Dustin.Hinkle.R@gmail.com>
2022-03-10 17:24:24 -0600

    Delete

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Los_Barrios_Project.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
