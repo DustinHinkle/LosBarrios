81249bb806f4aab833d566c44f7ccedcd71daad0
Dustin Hinkle <Dustin.Hinkle.R@gmail.com>
2022-04-04 12:28:52 -0500

    Taken Sample Code Still Stuck

---
+++ SemesterProject-Spring2022/webapp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using noaalib;
+
+namespace webapp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        NOAAADDSStationHelper helper = new NOAAADDSStationHelper();
+    }
+}
---
+++ SemesterProject-Spring2022/webapp/Services/EmailSender.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+
+using emaillib;
+
+
+namespace webapp.Services;
+
+public class EmailSender : IEmailSender
+{
+    private readonly ILogger _logger;
+    private  EmailSenderAdapter SenderAdapter {get; } = new EmailSenderAdapter();
+
+    public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+                       ILogger<EmailSender> logger)
+    {
+        Options = optionsAccessor.Value;
+        _logger = logger;
+    }
+
+    public AuthMessageSenderOptions Options { get; } //Set with Secret Manager.
+
+    public async Task SendEmailAsync(string toEmail, string subject, string message)
+    {
+        // if (string.IsNullOrEmpty(Options.SENDGRID_API_KEY))    ///commented out to make room for one line below to remove exception
+        if (string.IsNullOrEmpty(Options.SendGridKey))
+        {
+            throw new Exception("Null SendGridKey");
+        }
+        _logger.LogInformation("SENDGRID_API_KEY OK");
+
+        // if (string.IsNullOrEmpty(Options.EMAIL_FROM_ADDRESS))    ///commented out to make room for one line below to remove exception
+        if (string.IsNullOrEmpty(Options.SendGridKey))
+        {
+            throw new Exception("Null SendGridKey");
+        }        
+        _logger.LogInformation("EMAIL_FROM_ADDRESS OK");
+        
+
+        // this calls our seperate service
+        await SenderAdapter.SendEmailAsync(
+            Options.SENDGRID_API_KEY, 
+            subject, 
+            message, 
+            toEmail, 
+            Options.EMAIL_FROM_ADDRESS);
+    }
+}
